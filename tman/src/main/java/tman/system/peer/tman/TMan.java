5e9d0b790d4fff2d1b8675bc93ebff2a8d0e5609
Alexandre Tamborrino <tamborrino.alexandre@gmail.com>
2013-05-01 12:53:19 +0200

    merge

--- tman/src/main/java/tman/system/peer/tman/TMan.java
+++ tman/src/main/java/tman/system/peer/tman/TMan.java
@@ -35,7 +35,7 @@ public final class TMan extends ComponentDefinition {
 	private long period;
 	private PeerAddress self;
 	private int utility;
-	private List<PeerAddress> tmanPartners;
+	private List<PeerAddress> tmanPartners = new ArrayList<PeerAddress>();
 	private List<PeerAddress> cyclonPartners = new ArrayList<PeerAddress>();
 	private TManConfiguration tmanConfiguration;
 	private final int tmanPartnersSize = 5;
