8bab226c491620c5c2fb1bca6b659f501ad204d8
Noura Rabardeau <nrabardeau@gmail.com>
2013-05-01 12:51:47 +0200

    utility

--- tman/src/main/java/tman/system/peer/tman/TMan.java
+++ tman/src/main/java/tman/system/peer/tman/TMan.java
@@ -2,7 +2,6 @@ package tman.system.peer.tman;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,90 +26,96 @@ import cyclon.system.peer.cyclon.CyclonSamplePort;
 public final class TMan extends ComponentDefinition {
 
 	private static final Logger logger = LoggerFactory.getLogger(TMan.class);
-    Negative<TManSamplePort> tmanPartnersPort = negative(TManSamplePort.class);
-    Positive<CyclonSamplePort> cyclonSamplePort = positive(CyclonSamplePort.class);
-    Positive<Network> networkPort = positive(Network.class);
-    Positive<Timer> timerPort = positive(Timer.class);
-    private long period;
-    private PeerAddress self;
+	Negative<TManSamplePort> tmanPartnersPort = negative(TManSamplePort.class);
+	Positive<CyclonSamplePort> cyclonSamplePort = positive(CyclonSamplePort.class);
+	Positive<Network> networkPort = positive(Network.class);
+	Positive<Timer> timerPort = positive(Timer.class);
+	private long period;
+	private PeerAddress self;
 	private int utility;
 	private List<PeerAddress> tmanPartners;
 	private List<PeerAddress> cyclonPartners = new ArrayList<PeerAddress>();
-    private TManConfiguration tmanConfiguration;
+	private TManConfiguration tmanConfiguration;
 	private final int tmanPartnersSize = 5;
 
-    public class TManSchedule extends Timeout {
+	public class TManSchedule extends Timeout {
 
-        public TManSchedule(SchedulePeriodicTimeout request) {
-            super(request);
-        }
+		public TManSchedule(SchedulePeriodicTimeout request) {
+			super(request);
+		}
 
-//-------------------------------------------------------------------
-        public TManSchedule(ScheduleTimeout request) {
-            super(request);
-        }
-    }
+		// -------------------------------------------------------------------
+		public TManSchedule(ScheduleTimeout request) {
+			super(request);
+		}
+	}
 
-//-------------------------------------------------------------------	
-    public TMan() {
-        tmanPartners = new ArrayList<PeerAddress>();
+	// -------------------------------------------------------------------
+	public TMan() {
+		tmanPartners = new ArrayList<PeerAddress>();
 
-        subscribe(handleInit, control);
-        subscribe(handleRound, timerPort);
-        subscribe(handleCyclonSample, cyclonSamplePort);
-        subscribe(handleTManPartnersResponse, networkPort);
-        subscribe(handleTManPartnersRequest, networkPort);
-    }
-//-------------------------------------------------------------------	
-    Handler<TManInit> handleInit = new Handler<TManInit>() {
-        @Override
-        public void handle(TManInit init) {
+		subscribe(handleInit, control);
+		subscribe(handleRound, timerPort);
+		subscribe(handleCyclonSample, cyclonSamplePort);
+		subscribe(handleTManPartnersResponse, networkPort);
+		subscribe(handleTManPartnersRequest, networkPort);
+	}
 
-            self = init.getSelf();
+	// -------------------------------------------------------------------
+	Handler<TManInit> handleInit = new Handler<TManInit>() {
+		@Override
+		public void handle(TManInit init) {
+
+			self = init.getSelf();
 			utility = peerUtility(self);
-            tmanConfiguration = init.getConfiguration();
-            period = tmanConfiguration.getPeriod();
+			tmanConfiguration = init.getConfiguration();
+			period = tmanConfiguration.getPeriod();
 
-            SchedulePeriodicTimeout rst = new SchedulePeriodicTimeout(period, period);
-            rst.setTimeoutEvent(new TManSchedule(rst));
-            trigger(rst, timerPort);
+			SchedulePeriodicTimeout rst = new SchedulePeriodicTimeout(period, period);
+			rst.setTimeoutEvent(new TManSchedule(rst));
+			trigger(rst, timerPort);
 
-        }
-    };
-//-------------------------------------------------------------------	
-    Handler<TManSchedule> handleRound = new Handler<TManSchedule>() {
-        @Override
-        public void handle(TManSchedule event) {
-            Snapshot.updateTManPartners(self, tmanPartners);
+		}
+	};
+	// -------------------------------------------------------------------
+	Handler<TManSchedule> handleRound = new Handler<TManSchedule>() {
+		@Override
+		public void handle(TManSchedule event) {
+			Snapshot.updateTManPartners(self, tmanPartners);
 
-            // Publish sample to connected components
-            trigger(new TManSample(tmanPartners), tmanPartnersPort);            
-        }
-    };
-//-------------------------------------------------------------------	
-    Handler<CyclonSample> handleCyclonSample = new Handler<CyclonSample>() {
-        @Override
-        public void handle(CyclonSample event) {
+			// Publish sample to connected components
+			trigger(new TManSample(tmanPartners), tmanPartnersPort);
+		}
+	};
+	// -------------------------------------------------------------------
+	Handler<CyclonSample> handleCyclonSample = new Handler<CyclonSample>() {
+		@Override
+		public void handle(CyclonSample event) {
 			cyclonPartners = event.getSample();
 			if (!cyclonPartners.isEmpty()) {
 
 				PeerAddress partner = cyclonPartners.get(0);
-				Random random = new Random();
-				while (tmanPartners.contains(partner)) {
-					partner = cyclonPartners.get(random.nextInt(cyclonPartners.size()));
+				int counter = 1;
+				while (tmanPartners.contains(partner) && counter < cyclonPartners.size()) {
+					partner = cyclonPartners.get(counter);
+					counter++;
 				}
 
-				if ((tmanPartners.size() < tmanPartnersSize) && (!tmanPartners.contains(partner))) {
-					tmanPartners.add(partner);
-				} else {
-					int last = tmanPartners.size() - 1;
-					PeerAddress lessPreferred = tmanPartners.get(last);
-					tmanPartners.remove(last);
-					tmanPartners.add(last, prefer(partner, lessPreferred));
+				if (!tmanPartners.contains(partner)) {
+					if (tmanPartners.size() < tmanPartnersSize) {
+						tmanPartners.add(partner);
+					} else {
+						int last = tmanPartners.size() - 1;
+						PeerAddress lessPreferred = tmanPartners.get(last);
+						tmanPartners.remove(last);
+						tmanPartners.add(last, prefer(partner, lessPreferred));
+					}
 				}
+
 			} else {
 				trace("empty cyclon sample");
 			}
+
 
 			trace("tman partners: ");
 			StringBuilder sb = new StringBuilder();
@@ -120,34 +125,32 @@ public final class TMan extends ComponentDefinition {
 			}
 			trace(sb.toString());
 
-        }
-    };
-//-------------------------------------------------------------------	
-    Handler<ExchangeMsg.Request> handleTManPartnersRequest = new Handler<ExchangeMsg.Request>() {
-        @Override
-        public void handle(ExchangeMsg.Request event) {
+		}
+	};
+	// -------------------------------------------------------------------
+	Handler<ExchangeMsg.Request> handleTManPartnersRequest = new Handler<ExchangeMsg.Request>() {
+		@Override
+		public void handle(ExchangeMsg.Request event) {
 
-        }
-    };
-    
-    Handler<ExchangeMsg.Response> handleTManPartnersResponse = new Handler<ExchangeMsg.Response>() {
-        @Override
-        public void handle(ExchangeMsg.Response event) {
+		}
+	};
 
-        }
-    };
+	Handler<ExchangeMsg.Response> handleTManPartnersResponse = new Handler<ExchangeMsg.Response>() {
+		@Override
+		public void handle(ExchangeMsg.Response event) {
 
-	public PeerAddress prefer(PeerAddress p1, PeerAddress p2){
-    	int u1 = peerUtility(p1);
-    	int u2 = peerUtility(p2);
-		if ((u1 > utility && u2 < utility)
-				|| (Math.abs(u1 - utility) < Math.abs(u2 - utility))) {
-    		return p1;
-    	}
-    	else{
-    		return p2;
-    	}
-    }
+		}
+	};
+
+	public PeerAddress prefer(PeerAddress p1, PeerAddress p2) {
+		int u1 = peerUtility(p1);
+		int u2 = peerUtility(p2);
+		if ((u1 > utility && u2 < utility) || (Math.abs(u1 - utility) < Math.abs(u2 - utility))) {
+			return p1;
+		} else {
+			return p2;
+		}
+	}
 
 	public int peerUtility(PeerAddress peer) {
 		return (-1) * peer.getPeerAddress().getId();
